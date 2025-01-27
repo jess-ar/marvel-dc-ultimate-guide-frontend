eae845e6360ba591a479a9698bc4b1514b1163f6
jess-ar <jessica18_24@hotmail.com>
2025-01-27 21:58:34 +0100

    feat: Major redesign and functionality updates

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,45 +1,48 @@
 import LogoMarvel from '@/components/logos/LogoMarvel';
 import LogoDc from '@/components/logos/LogoDc';
 
-import backgroundImage from '/assets/images/characters/marvel/escudo.jpg';
-
 const HomePage = () => {
     return (
-        <div
-            className="relative flex flex-col items-center justify-between min-h-screen pb-8 mt-6 bg-center bg-cover pt-11"
-            style={{
-                backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.9) 20%, rgba(0, 0, 0, 0.6) 40%, rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 1) 100%), url(${backgroundImage})`,
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-            }}
-        >
-            <div className="relative z-20 flex flex-col items-center justify-between min-h-screen pt-16 pb-64">
-                <h1 className="flex flex-col items-center mt-8 mb-12 text-center text-white font-protest">
-                    <span className="flex items-center space-x-4">
-                        <LogoMarvel className="h-auto w-7 sm:w-9 md:w-24" />
-                        <span className="mx-2 text-3xl font-bold sm:text-4xl md:text-4xl">&</span>
-                        <LogoDc className="h-auto w-7 sm:w-9 md:w-24" />
-                        <span className="mx-1 text-3xl font-bold sm:text-4xl md:text-4xl">:</span>
-                    </span>
-                    <span className="mt-4 text-3xl sm:text-5xl md:text-5xl">Definitive guide</span>
-                </h1>
+        <div className="relative z-20 flex flex-col items-center justify-between min-h-screen pt-32 pb-64">
+            <h1 className="flex flex-col items-center mt-8 mb-12 text-center text-white font-bangers">
+                <span className="flex items-center space-x-4">
+                    <LogoMarvel />
+                    <span className="mx-2 font-sans text-3xl font-bold sm:text-4xl md:text-4xl">&</span>
+                    <LogoDc />
+                    <span className="mx-1 font-sans text-3xl bold text sm:text-4xl md:text-4xl">:</span>
+                </span>
+                <span className="mt-4 text-3xl sm:text-5xl md:text-5xl">Definitive guide</span>
+            </h1>
 
-                <div className="max-w-2xl p-4 leading-relaxed text-left text-white" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)' }}>
-                    <p className="mb-4 text-base sm:text-base">
-                        <strong>Didn&apos;t know</strong> there&apos;s a <strong>Marvel world</strong> and a <strong>DC world</strong>? That there are two huge comic book giants? Don&apos;t worry, <strong>you&apos;re in the right place!</strong>
-                    </p>
-                    <p className="mb-4 text-sm sm:text-base">
-                        With this <strong>ultimate guide</strong>, you&apos;ll finally be able to watch <strong>Marvel</strong> and <strong>DC</strong> movies knowing which universe each hero belongs to—plus, <strong>you&apos;ll have fun along the way!</strong> 😉
-                    </p>
-                    <p className="mb-4 text-sm sm:text-base">
-                        And if you already know your way around these vast universes, <strong>get ready to be amazed</strong> because this page is made for true fans. <strong>Dive in and enjoy! 🚀</strong>
-                    </p>
-                    
-                    <p className="mt-6 text-xs leading-normal sm:text-sm">
-                        <em>Psst...</em> You can <strong>search</strong> for your favorite hero using the search bar in the <strong>navbar</strong>, or check out the popular characters from both <strong>Marvel</strong> and <strong>DC</strong> in the menu! There’s so much to discover, <strong>have fun! 😎</strong>
-                    </p>
-                </div>
+            <div className="max-w-5xl p-4 font-sans text-base leading-relaxed text-left text-white lg:text-lg" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)' }}>
+                <p className="mb-4">
+                    <strong>Didn&apos;t know</strong> there&apos;s a <strong>Marvel world</strong> and a <strong>DC world</strong>? That there are two huge comic book giants? Don&apos;t worry, <strong>you&apos;re in the right place!</strong>
+                </p>
+                <p className="mb-4 ">
+                    With this <strong>ultimate guide</strong>, you&apos;ll finally be able to watch <strong>Marvel</strong> and <strong>DC</strong> movies knowing which universe each hero belongs to—plus, <strong>you&apos;ll have fun along the way!</strong> 😉
+                </p>
+                <p className="mb-4 ">
+                    And if you already know your way around these vast universes, <strong>get ready to be amazed</strong> because this page is made for true fans. <strong>Dive in and enjoy! 🚀</strong>
+                </p>
+
+                <p className="mt-6 text-xs leading-normal lg:text-sm">
+                    <em>Psst...</em> You can <strong>search</strong> for your favorite hero using the search bar in the <strong>navbar</strong>, or check out the popular characters from both <strong>Marvel</strong> and <strong>DC</strong> in the menu! There’s so much to discover, <strong>have fun! 😎</strong>
+                </p>
+                <section className="flex items-center justify-center mt-8 space-x-4">
+                    <button
+                        className="px-6 py-3 text-lg font-bold transition-transform transform bg-red-600 rounded-full shadow-lg hover:bg-red-700 hover:scale-105"
+                        onClick={() => window.location.href = '/marvel'}
+                    >
+                        Explore Marvel
+                    </button>
+                    <button
+                        className="px-6 py-3 text-lg font-bold transition-transform transform bg-blue-600 rounded-full shadow-lg hover:bg-blue-700 hover:scale-105"
+                        onClick={() => window.location.href = '/dc'}
+                    >
+                        Explore DC
+                    </button>
+                </section>
+
             </div>
         </div>
     );
--- src/pages/profile/ProfileWelcome.jsx
+++ src/pages/profile/ProfileWelcome.jsx
@@ -5,19 +5,19 @@ import { useEffect } from 'react';
 
 const backgroundImage = '/assets/images/characters/marvel/spider-back.jpg';
 
-const ProfilePage = () => {
+const ProfileWelcome = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
         const token = getToken();
         if (token) {
-            navigate('/user/profile');
+            navigate('/welcome');
         }
     }, [navigate]);
 
     return (
         <div
-            className="relative flex flex-col items-center justify-end h-screen bg-center bg-cover md:bg-top lg:bg-center xl:bg-cover"
+            className="relative flex flex-col items-center justify-end h-screen mt-10 bg-center bg-cover"
             style={{
                 backgroundImage: `url(${backgroundImage})`,
             }}
@@ -30,8 +30,8 @@ const ProfilePage = () => {
             ></div>
 
             <div className="relative z-20 flex flex-col items-center justify-center mb-24 space-y-6">
-                <h1 className="text-lg font-bold text-white md:text-lg lg:text-4xl">Welcome to Your Profile</h1>
-                <p className="max-w-xl text-base text-center text-gray-300 md:text-base lg:text-[1.0925rem]">
+                <h1 className="text-lg font-bold text-white font-bangers md:text-lg lg:text-4xl">Welcome to Your Profile</h1>
+                <p className="max-w-xl text-base font-sans text-center text-gray-300 md:text-base lg:text-[1.0925rem]">
                     Explore your personalized space in the Marvel/DC app. Log in to access your favorite heroes and manage your profile.
                 </p>
 
@@ -52,4 +52,4 @@ const ProfilePage = () => {
     );
 };
 
-export default ProfilePage;
+export default ProfileWelcome;
