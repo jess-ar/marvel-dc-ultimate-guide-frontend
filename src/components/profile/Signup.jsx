e115e00426b96dc09997ce17a3a4a9075d96e82a
jess-ar <jessica18_24@hotmail.com>
2025-01-29 21:40:59 +0100

    Minor UI improvements

--- src/components/profile/Signup.jsx
+++ src/components/profile/Signup.jsx
@@ -81,7 +81,7 @@ const SignUp = () => {
                     <LogoDc className="w-20 md:w-24 lg:w-28" />
                 </div>
 
-                <h2 className="w-full mt-2 mb-12 text-2xl font-bold text-left md:text-4xl font-bangers text-secondary">Sign up</h2>
+                <h2 className="w-full mt-2 mb-12 text-2xl font-bold text-left md:text-4xl font-bangers text-secondary">Sign Up</h2>
                 <div className="relative w-full mb-1">
                     {error && (
                         <p className="mb-4 text-center text-primary">
@@ -143,7 +143,7 @@ const SignUp = () => {
                         <div className="w-full mt-6 mb-4 text-sm text-center text-secondary">
                             Already have an account?{' '}
                             <a onClick={() => navigate('/login')} className="cursor-pointer text-primary hover:underline">
-                                Log in
+                                Log In
                             </a>
                         </div>
                     </form>
